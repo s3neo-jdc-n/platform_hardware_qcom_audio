1bb7294b83acb71981534d4e87a1a848e85a1b67
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-26 22:36:21 +0200

    audiod: remove clean a bit the mk
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- audiod/Android.mk
+++ audiod/Android.mk
@@ -2,10 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifneq (,$(findstring $(PLATFORM_VERSION), 5.0 5.1 5.1.1))
-include external/stlport/libstlport.mk
-endif
-
 LOCAL_SRC_FILES:= \
 	audiod_main.cpp \
 	AudioDaemon.cpp \
@@ -18,10 +14,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libbinder \
 	libmedia \
 	liblog
-
-ifneq (,$(findstring $(PLATFORM_VERSION), 5.0 5.1 5.1.1))
-LOCAL_SHARED_LIBRARIES += libstlport
-endif
 
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
